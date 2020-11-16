bc4fe7be5f8f9fb5fb975cc816df727e8f816939
Алексей <50051634+Deep3reath@users.noreply.github.com>
2020-11-16 15:20:17 +0700

    Правки
    Правки миграций и некоторых функций

--- database/migrations/2014_10_12_000000_create_user_table.php
+++ database/migrations/2014_10_12_000000_create_user_table.php
@@ -15,7 +15,7 @@ class CreateUserTable extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
